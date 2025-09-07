9a4f17fb91c9d7e45a3d76d312b8fe3352034e96
Vladimirs Gadirovs <vladimirskeletons@gmail.com>
2025-09-07 21:17:54 +0300

    Сиськи Рем на шарпах
    )))

---
+++ SiskiRemCharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"(.)(.) - Это сиськи Рем. Они больше, чем у Рам. Размер:{Rem.BoobaSize}");
+        Console.WriteLine($"(.)(.) - Это сиськи Рам. Они меньше, чем у Рем. Размер:{Ram.BoobaSize}");
+        Console.Read();
+    }
+}
+
+public abstract class Human { }
+public abstract class Men:Human { }
+public abstract class Woman:Men
+{
+    public static float BoobaSize { get; private set; } = 0.0f;
+}
+public class Ram:Woman
+{
+    public static new float BoobaSize { get; private set; } = 2.0f;
+}
+
+public class Rem:Woman
+{
+    public static new float BoobaSize => Ram.BoobaSize * 1.5f; 
+}
