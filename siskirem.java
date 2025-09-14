f7ec96673b82f06ba72e7cee2a90d67a8a7e6d40
rost <alexeevha39@gmail.com>
2025-09-14 11:21:03 +0200

    java upd

--- siskirem.java
+++ siskirem.java
@@ -6,10 +6,12 @@ enum Gender {
 
 abstract class Human {
     public Gender gender = Gender.UNDEFINED;
+    public float BoobSize = 0.0f;
 }
 
 abstract class Man extends Human {
     public Gender gender = Gender.MALE;
+    public float BoobSize = 0.75f;
 }
 
 abstract class Woman extends Man {
@@ -19,10 +21,17 @@ abstract class Woman extends Man {
 
 class Ram extends Woman {
     public float BoobSize = 2.0f;
+    public void OnInteract() {
+        System.out.println("ram interaction placeholder");
+    }
 }
 
 class Rem extends Ram {
     public float BoobSize = super.BoobSize * 1.5f;
+    @Override
+    public void OnInteract() {
+        System.out.println("rem interaction placeholder");
+    }
 }
 
 public class siskirem {
